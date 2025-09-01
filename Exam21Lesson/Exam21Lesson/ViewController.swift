45e7688eb7663e17693e5d6ed8c75208ef1c5a2d
Kaimov-i <117822275+Kaimov-i@users.noreply.github.com>
2025-09-01 06:19:11 +0300

    Creating project

---
+++ Exam21Lesson/Exam21Lesson/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Exam21Lesson
+//
+//  Created by Ilman on 01.09.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .gray
+    }
+
+
+}
+
